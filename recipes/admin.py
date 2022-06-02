69bcc7c6905f4bffd18ecff9560c7deb5e8b4c9e
Alex Porter <alpo1992@gmail.com.here>
2022-06-02 16:11:33 -0600

    .

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from recipes.models import Recipe, Step, Measure, FoodItem
+from recipes.models import Recipe, Step, Measure, FoodItem, Ingredient
 
 # Register your models here.
 
@@ -20,7 +20,12 @@ class FoodItemAdmin(admin.ModelAdmin):
     pass
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Step, StepAdmin)
 admin.site.register(Measure, MeasureAdmin)
 admin.site.register(FoodItem, FoodItemAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -18,15 +18,38 @@ class Recipe(models.Model):
 class Step(models.Model):
     order = models.SmallIntegerField()
     directions = models.TextField()
-    author = models.ForeignKey(
+    recipe = models.ForeignKey(
         "Recipe", related_name="steps", on_delete=models.CASCADE
     )
+    food_items = models.ManyToManyField("FoodItem", null=True, blank=True)
 
 
 class Measure(models.Model):
     name = models.CharField(max_length=30, unique=True)
     abbreviation = models.CharField(max_length=10, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class FoodItem(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    amount = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    recipe = models.ForeignKey(
+        "Recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+    measure = models.ForeignKey(
+        "Measure", related_name="measures", on_delete=models.PROTECT
+    )
+    food = models.ForeignKey("FoodItem", on_delete=models.PROTECT)
