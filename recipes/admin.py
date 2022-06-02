8f813add0ed33afb0f6bb078dd54d79e4bb08147
Alex Porter <alpo1992@gmail.com.here>
2022-06-02 12:54:57 -0600

    .

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-
+from recipes.models import Recipe, Step, Measure, FoodItem
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Step, StepAdmin)
+admin.site.register(Measure, MeasureAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,31 @@ from django.db import models
 
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " by " + self.author
+
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Step(models.Model):
+    order = models.SmallIntegerField()
+    directions = models.TextField()
+    author = models.ForeignKey(
+        "Recipe", related_name="steps", on_delete=models.CASCADE
+    )
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
